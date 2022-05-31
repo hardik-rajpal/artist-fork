21931dc3678ed3c4769b57536a7037d18910b94a
Hardik <hardikraj08@gmail.com>
2022-05-31 17:32:40 +0530

    ...

--- examples/ball.cpp
+++ examples/ball.cpp
@@ -1,100 +1,99 @@
-/*=============================================================================
-   Copyright (c) 2016-2020 Joel de Guzman
-
-   Distributed under the MIT License [ https://opensource.org/licenses/MIT ]
-=============================================================================*/
 #include "app.hpp"
 #include <cstdlib>
-#include<vector>
-#include<unistd.h>
-#include<iostream>
-#include<thread>
+#include <vector>
+#include <unistd.h>
+#include <iostream>
+#include <thread>
 using namespace cycfi::artist;
-#include"host/linux/cycoop/CycLib.h"
-
-///////////////////////////////////////////////////////////////////////////////
-// Ported from Rainbow Rain animation:
-// https://onaircode.com/awesome-html5-canvas-examples-source-code/
-///////////////////////////////////////////////////////////////////////////////
+#include "host/linux/cycoop/CycLib.h"
 
 #if defined(ARTIST_SKIA) && !defined(linux)
 constexpr auto persistence = 0.10;
 #else
 constexpr auto persistence = 0.04;
 #endif
-constexpr auto window_size = extent{ 900, 600 };
-constexpr float radius = 10;
-float posx = 100;
-float posy = 100;
-int velx[5];
-int vely[5];
-float opacity = 1.0;
-int numRuns = 0;
-int rando(int m, int M){
+constexpr auto window_size = extent{1500, 800};
+int rando(int m, int M)
+{
     int h;
-    while(true){
+    while (true)
+    {
         h = rand();
-        if (m<h and h<M){
+        if (m < h and h < M)
+        {
             return h;
         }
     }
 }
-void branch(int lob, CycTurtle mm){
-    if (lob/80 >0) {
-        mm.rotateBy(rando(0,120)-60);
-        for(int j=0;j<lob/40;j++){
-            mm.rotateBy(rando(0, 30)-15);
+void branch(int lob, CycTurtle mm)
+{
+    if (lob / 80 > 0)
+    {
+        mm.rotateBy(rando(0, 120) - 60);
+        for (int j = 0; j < lob / 40; j++)
+        {
+            mm.rotateBy(rando(0, 30) - 15);
             mm.forward(20);
             float x1 = mm.cx, y1 = mm.cy;
             int subb = rando(0, 30);
-            mm.rotateBy(subb); branch(lob/8, mm);mm.rotateBy(-subb);
+            mm.rotateBy(subb);
+            branch(lob / 8, mm);
+            mm.rotateBy(-subb);
             mm.moveTo(x1, y1);
             mm.forward(20);
             x1 = mm.cx, y1 = mm.cy;
             subb = rando(0, 30);
-            mm.rotateBy(-subb); branch(lob/8, mm);mm.rotateBy(subb);
+            mm.rotateBy(-subb);
+            branch(lob / 8, mm);
+            mm.rotateBy(subb);
             mm.moveTo(x1, y1);
-            if (mm.scale<0.6){
+            if (mm.scale < 0.6)
+            {
                 mm.scaleTo(1.5);
             }
-
         }
     }
-    else{
-         mm.rotateBy(rando(0, 120)-60);
-        for(int j=0;j<lob/20;j++){
-            mm.rotateBy(rando(0, 30)-15);
+    else
+    {
+        mm.rotateBy(rando(0, 120) - 60);
+        for (int j = 0; j < lob / 20; j++)
+        {
+            mm.rotateBy(rando(0, 30) - 15);
             mm.forward(20);
             int subb = rando(0, 30);
-            mm.rotateBy(subb); mm.forward(10); mm.stamp(); mm.forward(-5);mm.rotateBy(-subb);
+            mm.rotateBy(subb);
+            mm.forward(10);
+            mm.stamp();
+            mm.forward(-5);
+            mm.rotateBy(-subb);
             mm.forward(20);
             subb = rando(0, 30);
-            mm.rotateBy(-subb); mm.forward(5); mm.stamp(); mm.forward(-5);mm.rotateBy(subb);
+            mm.rotateBy(-subb);
+            mm.forward(5);
+            mm.stamp();
+            mm.forward(-5);
+            mm.rotateBy(subb);
         }
     }
 }
-int main(int argc, char const* argv[])
+int main(int argc, char const *argv[])
 {
-   srand(0);
-   CycCanvas c(argc, argv, window_size,colors::pink);   
-   CycTurtle t(c,100,50);
-//    float rad = 100;
-//    float theta = 30;
-//    // CycRect r(c,100,100,100,100);
-//    t.scaleTo(0.1);
-//    t.rotateBy(80);
-//    t.moveTo(750, 700);
-//    t.togglePen();
-//    int x0 = t.cx, y0 = t.cy;
-//    t.rotateBy(90);
-//    t.forward(100);
-//    branch(400, t);
-//    t.moveTo(x0, y0);
-//    t.rotateBy(-30);
-   while(c.globStatus!=0){
-       t.rotateBy(10);
-      usleep(100000);
-   }
-   
+    srand(0);
+    CycCanvas c(argc, argv, window_size, colors::pink);
+    CycTurtle t(c, 100, 50);
+    t.scaleTo(0.1);
+    t.setPenType(2, cycfi::artist::colors::brown);
+    t.setStrokeWidth(2);
+    t.moveTo(750, 700);
+    t.togglePen();
+    int x0 = t.cx, y0 = t.cy;
+    t.forward(100);
+    branch(200, t);
+    t.moveTo(x0, y0);
+    t.rotateBy(90);
+    while (c.globStatus != 0)
+    {
+        // t.rotateBy(10);
+        usleep(100000);
+    }
 }
-
--- examples/host/linux/cycoop/CycShape.cpp
+++ examples/host/linux/cycoop/CycShape.cpp
@@ -1,122 +1,145 @@
-#include"CycShape.h"
-#include<math.h>
-#include<utility>
-#include<iostream>
-float degToRad(float deg){
-    return (deg/180.0)*M_PI;
+#include "CycShape.h"
+#include <math.h>
+#include <utility>
+#include <iostream>
+float degToRad(float deg)
+{
+    return (deg / 180.0) * M_PI;
 }
-std::pair<float,float> thetaToSinCos(float thetaDeg){
-    return std::pair(cos(degToRad(thetaDeg)),sin(degToRad(thetaDeg)));
+std::pair<float, float> thetaToSinCos(float thetaDeg)
+{
+    return std::pair<float, float>(cos(degToRad(thetaDeg)), sin(degToRad(thetaDeg)));
 }
 
-CycShape::CycShape(CycCanvas &cyccnv,float cx, float cy):CycObject(cyccnv,cx,cy){}
+CycShape::CycShape(CycCanvas &cyccnv, float cx, float cy) : CycObject(cyccnv, cx, cy) {}
 
-CycShape::CycShape(CycCanvas &cyccnv,float cx, float cy, cycfi::artist::color fillColor,cycfi::artist::color strokeColor):CycObject(cyccnv,cx,cy){
-    this->fillColor=fillColor;
-    this->strokeColor=strokeColor;
+CycShape::CycShape(CycCanvas &cyccnv, float cx, float cy, cycfi::artist::color fillColor, cycfi::artist::color strokeColor) : CycObject(cyccnv, cx, cy)
+{
+    this->fillColor = fillColor;
+    this->strokeColor = strokeColor;
 }
-void CycShape::fill(art::color fillColor){
-    this->fillColor=fillColor;
+void CycShape::fill(art::color fillColor)
+{
+    this->fillColor = fillColor;
 }
-void CycShape::stroke(art::color strokeColor){
-    this->strokeColor=strokeColor;
+void CycShape::stroke(art::color strokeColor)
+{
+    this->strokeColor = strokeColor;
 }
-void CycShape::setStrokeWidth(float strokeWidth){
-    this->strokeWidth=strokeWidth;
+void CycShape::setStrokeWidth(float strokeWidth)
+{
+    this->strokeWidth = strokeWidth;
 }
-CycCircle::CycCircle(CycCanvas &cyccnv,float cx, float cy, float radius,art::color fillColor,art::color strokeColor):CycShape(cyccnv,cx,cy, fillColor,strokeColor){
+CycCircle::CycCircle(CycCanvas &cyccnv, float cx, float cy, float radius, art::color fillColor, art::color strokeColor) : CycShape(cyccnv, cx, cy, fillColor, strokeColor)
+{
     this->radius = radius;
 }
-CycCircle::CycCircle(CycCanvas &cyccnv,float cx, float cy, float radius):CycShape(cyccnv,cx,cy){
+CycCircle::CycCircle(CycCanvas &cyccnv, float cx, float cy, float radius) : CycShape(cyccnv, cx, cy)
+{
     this->radius = radius;
 }
-void CycCircle::render(art::canvas &cnv){
+void CycCircle::render(art::canvas &cnv)
+{
     cnv.line_width(this->strokeWidth);
     cnv.stroke_color(strokeColor);
-    cnv.arc(this->cx, this->cy,radius,0.0f,360.0f,false);
+    cnv.arc(this->cx, this->cy, radius, 0.0f, 360.0f, false);
     cnv.fill_style(fillColor);
     cnv.fill();
-    cnv.arc(this->cx, this->cy,radius,0.0f,360.0f,false);
+    cnv.arc(this->cx, this->cy, radius, 0.0f, 360.0f, false);
     cnv.stroke();
 }
-bool CycCircle::inRange(float x, float y){
-    return sqrt((x-cx)*(x-cx)+(y-cy)*(y-cy))<(radius+strokeWidth);
+bool CycCircle::inRange(float x, float y)
+{
+    return sqrt((x - cx) * (x - cx) + (y - cy) * (y - cy)) < (radius + strokeWidth);
 }
-CycRect::CycRect(CycCanvas &cyccnv,float cx, float cy, float width, float height):CycShape(cyccnv,cx, cy){
-    this->width = width;this->height = height;
+CycRect::CycRect(CycCanvas &cyccnv, float cx, float cy, float width, float height) : CycShape(cyccnv, cx, cy)
+{
+    this->width = width;
+    this->height = height;
 }
-void CycRect::render(art::canvas &cnv){
-    float tx = cx - width/2;
-    float ty = cy - height/2;
+void CycRect::render(art::canvas &cnv)
+{
+    float tx = cx - width / 2;
+    float ty = cy - height / 2;
     cnv.line_width(this->strokeWidth);
     cnv.stroke_color(strokeColor);
-    cnv.stroke_rect(tx,ty,width,height);
+    cnv.stroke_rect(tx, ty, width, height);
     cnv.fill_style(fillColor);
-    cnv.fill_rect(tx,ty, width,height);
+    cnv.fill_rect(tx, ty, width, height);
     cnv.fill();
     cnv.stroke();
 }
-bool CycRect::inRange(float x, float y){
-    return (abs(cx-x)<(width+strokeWidth)/2)&&(abs(y-cy)<(height+strokeWidth)/2);
+bool CycRect::inRange(float x, float y)
+{
+    return (abs(cx - x) < (width + strokeWidth) / 2) && (abs(y - cy) < (height + strokeWidth) / 2);
 }
-CycPoly::CycPoly(CycCanvas &cyccnv,float cx, float cy, float radius, int numsides):CycShape(cyccnv,cx, cy){
+CycPoly::CycPoly(CycCanvas &cyccnv, float cx, float cy, float radius, int numsides) : CycShape(cyccnv, cx, cy)
+{
     this->radius = radius;
     this->numsides = numsides;
 }
-void CycPoly::render(art::canvas &cnv){
-    
+void CycPoly::render(art::canvas &cnv)
+{
 }
-CycTurtle::CycTurtle(CycCanvas &cyccnv, float cx, float cy):CycShape(cyccnv,cx,cy,cycfi::artist::colors::red,cycfi::artist::colors::black){
+CycTurtle::CycTurtle(CycCanvas &cyccnv, float cx, float cy) : CycShape(cyccnv, cx, cy, cycfi::artist::colors::red, cycfi::artist::colors::black)
+{
     strokeWidth = 3;
 }
-void CycTurtle::render(art::canvas &cnv){
-    float inrad = scale*edgeLength/sqrt(3);
+void CycTurtle::render(art::canvas &cnv)
+{
+    float inrad = scale * edgeLength / sqrt(3);
     cnv.line_width(strokeWidth);
     cnv.stroke_color(strokeColor);
     cnv.begin_path();
-    std::pair<float,float> cs = thetaToSinCos(theta);
-    cnv.move_to(cx+(inrad*cs.first),cy+(inrad*cs.second));
-    cs = thetaToSinCos(120+theta);
-    cnv.line_to(cx+(inrad*cs.first),cy+(inrad*cs.second));
-    cs = thetaToSinCos(240+theta);
-    cnv.line_to(cx+(inrad*cs.first),cy+(inrad*cs.second));
+    std::pair<float, float> cs = thetaToSinCos(theta);
+    cnv.move_to(cx + (inrad * cs.first), cy + (inrad * cs.second));
+    cs = thetaToSinCos(120 + theta);
+    cnv.line_to(cx + (inrad * cs.first), cy + (inrad * cs.second));
+    cs = thetaToSinCos(240 + theta);
+    cnv.line_to(cx + (inrad * cs.first), cy + (inrad * cs.second));
     cnv.fill_style(fillColor);
     cnv.close_path();
     cnv.fill();
 
     cnv.begin_path();
     cs = thetaToSinCos(theta);
-    cnv.move_to(cx+(inrad*cs.first),cy+(inrad*cs.second));
-    cs = thetaToSinCos(120+theta);
-    cnv.line_to(cx+(inrad*cs.first),cy+(inrad*cs.second));
-    cs = thetaToSinCos(240+theta);
-    cnv.line_to(cx+(inrad*cs.first),cy+(inrad*cs.second));
+    cnv.move_to(cx + (inrad * cs.first), cy + (inrad * cs.second));
+    cs = thetaToSinCos(120 + theta);
+    cnv.line_to(cx + (inrad * cs.first), cy + (inrad * cs.second));
+    cs = thetaToSinCos(240 + theta);
+    cnv.line_to(cx + (inrad * cs.first), cy + (inrad * cs.second));
     cnv.fill_style(fillColor);
     cnv.close_path();
     cnv.stroke();
-
-}   
-bool CycTurtle::inRange(float x, float y){
+}
+bool CycTurtle::inRange(float x, float y)
+{
     return true;
 }
-void CycTurtle::forward(float dx){
-    std::pair<float,float> cs = thetaToSinCos(theta);
-    moveBy(cs.first*dx,cs.second*dx);
+void CycTurtle::forward(float dx)
+{
+    std::pair<float, float> cs = thetaToSinCos(theta);
+    moveBy(cs.first * dx, cs.second * dx);
 }
-void CycTurtle::rotateBy(float dTheta){
-    theta+=dTheta;
+void CycTurtle::rotateBy(float dTheta)
+{
+    theta += dTheta;
 }
-void CycTurtle::rotateTo(float theta){
+void CycTurtle::rotateTo(float theta)
+{
     this->theta = theta;
 }
-void CycTurtle::stamp(){
-    CycTurtle * stamp = new CycTurtle(*cyccnv,cx,cy);
+void CycTurtle::stamp()
+{
+    CycTurtle *stamp = new CycTurtle(*cyccnv, cx, cy);
     stamp->rotateTo(theta);
     stamp->scaleTo(scale);
 }
-void CycTurtle::scaleBy(float dScale){
-    scale*=dScale;
+void CycTurtle::scaleBy(float dScale)
+{
+    scale *= dScale;
 }
-void CycTurtle::scaleTo(float newScale){
+void CycTurtle::scaleTo(float newScale)
+{
     scale = newScale;
 }
